c391996661fa3b98d60bca456c0f08a086bdde11
User_3 <user3@example.com>
2026-05-16 10:55:54 +0530

    Fixed learning resources bug

--- modules/learning_resources.py
+++ modules/learning_resources.py
@@ -5,7 +5,8 @@ def get_learning_resources(career_name):
     with open("data/courses.json", "r") as file:
         courses = json.load(file)
 
-    if career_name in courses:
-        return courses[career_name]
-
-    return {}
+    career_name=career_name.strip()
+    for career in courses:
+        if career.strip().lower() == career_name.lower():
+            return courses[career]
+        return {}
